eed48c1dd7c7f5c31bc1769c23617312ddcaf955
ohtaehyun <othdev95@gmail.com>
2022-08-31 21:13:11 +0900

    feat: theme 파일 분리

--- src/index.tsx
+++ src/index.tsx
@@ -3,56 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import rootReducer from "./store";
 import {Provider} from "react-redux";
 import { configureStore } from '@reduxjs/toolkit';
+import theme from './theme';
 
 const store = configureStore({reducer: rootReducer});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-const theme = createTheme({
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        '*': {
-          boxSizing: 'border-box',
-          margin: 0,
-          padding: 0
-        },
-        html: {
-          height: '100%',
-          width: '100%'
-        },
-        body: {
-          height: '100%',
-          width: '100%'
-        },
-        '#root': {
-          height: '100%',
-          width: '100%'
-        }
-      }
-    }
-  },
-  palette: {
-    primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000'
-    }
-  }
-});
 
 root.render(
   <React.StrictMode>
